263c26af7cbdb7c8a568cdb369f74166b2ecdb25
Adam Goth <adamgoth@gmail.com>
2017-02-26 12:06:17 -0700

    Adds challenge to reverse an array

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -6,7 +6,11 @@ namespace Challenges
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//Console.WriteLine("Hello World!");
+
+			string[] array = { "1", "2", "3" };
+			Challenges.ReverseArray.ReverseArrayMethod(array);
 		}
+
 	}
 }
---
+++ Challenges/ReverseArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Challenges
+{
+	public class ReverseArray
+	{
+		public static void ReverseArrayMethod(Array array)
+		{
+			Console.Write("Reversed Array: ");
+			Array.Reverse(array);
+			foreach (var item in array)
+				Console.Write(item + " ");
+		}
+	}
+}
